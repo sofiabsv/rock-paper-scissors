4761ef5b871ddd8cd3272d24d0aa781f9af6a4a4
Sofia Berdichevska <sofiaxberdichevska@gmail.com>
2024-05-28 19:36:43 +0200

    Created getHumanChoice

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+  let choice = prompt("Please choose rock, paper or scissors.");
+
+  if (choice == "rock") {
+    return "You chose rock.";
+  } else if (choice == "paper") {
+    return "You chose paper.";
+  } else if (choice == "scissors") {
+    return "You chose scissors";
+  } else {
+    return "Invalid Input.";
+  }
+}
+
+console.log(getHumanChoice());
